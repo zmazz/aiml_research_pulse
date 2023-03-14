99ff4f4b66b814a1e016eed617c3a8b075eedec3
zmazz <78746857+zmazz@users.noreply.github.com>
2023-03-14 17:31:43 +0100

    front work on research

--- research_pulse/interface/app.py
+++ research_pulse/interface/app.py
@@ -13,7 +13,6 @@ import streamlit.components.v1 as components
 from base64 import b64encode
 from io import BytesIO
 
-
 st.set_page_config(
     page_title="ResPulse",
     page_icon=":smiley:",
@@ -221,14 +220,14 @@ with Research:
                         if pdf_response.status_code == 200:
                             # Read the downloaded binary data into a BytesIO object
                             pdf_data = BytesIO(pdf_response.content)
-                            # Generate the HTML code to display the PDF
-                            base64_pdf = b64encode(pdf_data.read()).decode('utf-8')
-                            pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-                            # Display the PDF
-                            st.markdown(pdf_display, unsafe_allow_html=True)
+
                         else:
                             st.error('Failed to download PDF')
-
+                        with open(pdf_data,"rb") as f:
+                            base64_pdf = b64encode(f.read()).decode('utf-8')
+                        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="600" height="800" type="application/pdf">'
+                        st.markdown(pdf_display, unsafe_allow_html=True)
+                        # Opening file from file path
 
     with Author_details:
         with st.form(key='params_for_api_research_author'):
