39d4b1df26aadf84b3f2e66097bc2e3b1a6e1c5b
zmazz <78746857+zmazz@users.noreply.github.com>
2023-03-14 16:39:34 +0100

    front work on research

--- research_pulse/interface/app.py
+++ research_pulse/interface/app.py
@@ -207,7 +207,7 @@ with Research:
                         st.text(' ')
                 with col8 :
                     for key in results3:
-                        pdf_url = results3[key]['Link']+"#toolbar=0"
+                        pdf_url = results3[key]['Link']
                         # Use pdfjs to display the PDF
                         pdf_viewer = iframe(src=pdf_url, width=600, height=800)
                         # Display the PDF viewer
